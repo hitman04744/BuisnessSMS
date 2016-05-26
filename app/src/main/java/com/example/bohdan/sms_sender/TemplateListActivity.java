aa64eda2970ecc269ff007af854d443a9b30d6ea
Bohdan <hitman04744@gmail.com>
2016-05-26 22:33:57 +0300

    First Commit

---
+++ app/src/main/java/com/example/bohdan/sms_sender/TemplateListActivity.java
@@ -0,0 +1,13 @@
+package com.example.bohdan.sms_sender;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TemplateListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template_list);
+    }
+}
